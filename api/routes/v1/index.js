cb7363ae77312588943dfcbae03598f9fb8e0a8c
Jimmy Huynh <jkchuynh829@gmail.com>
2017-10-21 16:56:21 -0700

    Update node scripts

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const userService = require('../../services/userService');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello!');
-});
+router.get('/users/:id', userService.getUser);
 
 module.exports = router;
