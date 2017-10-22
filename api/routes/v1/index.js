d9dcf8ba1f1bdfb89b002ed484adf7c175ff961f
Michael Bieronski <mike@Michaels-MacBook-Pro-3.local>
2017-10-22 00:24:20 -0700

    added submit service

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const userService = require('../../services/userService');
-
-const fs = require('fs');
-const {join}= require('path');
-const router = express.Router();
-const RequestService = require("./../../services/requestService")
+const express = require('express'),
+      userService = require('../../services/userService'),
+      SubmitService = require('../../services/submitService'),
+      RequestService = require("./../../services/requestService"),
+      fs = require('fs'),
+      {join}= require('path'),
+      router = express.Router();
 
 router.get('/users/:id', userService.getUser);
 
 router.post('/requests', RequestService.createSession);
 
 router.get('/requests/:safetyCode', RequestService.getSession);
+
+router.post('/submit', SubmitService.submitResponse);
+
+
 module.exports = router;
