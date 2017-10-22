75c275d09746e178d266748578c337a539807c89
Jimmy Huynh <jkchuynh829@gmail.com>
2017-10-22 02:09:52 -0700

    Some more linting

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const UserService = require('../../services/userService');
 const SubmitService = require('../../services/submitService');
 const RequestService = require('./../../services/requestService');
@@ -6,9 +7,11 @@ const RequestService = require('./../../services/requestService');
 const router = express.Router();
 
 router.get('/users/:id', UserService.getUser);
+
 router.post('/users/card', UserService.getUserDetail);
 
 router.get('/requests/:safetyCode', RequestService.getSession);
+
 router.post('/requests', RequestService.createSession);
 
 router.post('/submit', SubmitService.submitResponse);
