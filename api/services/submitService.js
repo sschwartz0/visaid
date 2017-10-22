14ffdce3d0cd135d02122ccffcd5bf4a211f2ac4
Michael Bieronski <mike@Michaels-MacBook-Pro-3.local>
2017-10-22 10:03:39 -0700

    added permissions

--- api/services/submitService.js
+++ api/services/submitService.js
@@ -8,6 +8,7 @@ const submitResponse = function (req, res) {
   fs.readFile(join(__dirname, fileName), 'utf-8', (err, requestsDB) => {
     requestsDB = JSON.parse(requestsDB);
     const session = requestsDB[req.body.safetyCode];
+    let sentPermissions = requestsDB[req.body.safetyCode].permissions;
     if (!session) res.status(500).send('no active sessions found with that code');
     Object.keys(session.permissions).forEach(name => {
       session.permissions[name] = req.body.permissions[name];
@@ -22,8 +23,13 @@ const submitResponse = function (req, res) {
             newDB = JSON.parse(newDB);
             session.userData = {};
             Object.keys(session.permissions).forEach(permission => {
-              session.userData[permission] = formatData(user, permission);
+              session.userData[permission] = 1;//formatData(user, permission);
+              delete sentPermissions[permission];
             });
+            Object.keys(sentPermissions).forEach(ele => {
+                session.userData[ele] = null;
+            });
+            console.log(session)
             session.status = 'COMPLETE';
             fs.writeFile(join(__dirname, fileName), JSON.stringify(requestsDB, null, 2), (err) => {
               if (err) res.status(500).send(err);
@@ -37,7 +43,7 @@ const submitResponse = function (req, res) {
 };
 
 const formatData = function (responseOb, permission) {
-  if (permission === 'name') return responseOb.firstName + responseOb.lastName;
+  if (permission === 'Name') return responseOb.firstName + responseOb.lastName;
   if (permission === 'address') {
     const ob = Object.assign({}, responseOb.resource.address);
     delete ob.addressLine3;
@@ -45,6 +51,7 @@ const formatData = function (responseOb, permission) {
     return Object.keys(ob).reduce((acc, ele) => `${acc} ${ob[ele]}`, '');
   }
   if (permission === 'mobileNumber' || permission === 'homeNumber') {
+      permission = responseOb.resource.homeNumber;
     return `1 (${permission.slice(0, 2)}) ${permission.slice(2, 5)}-${permission.slice(5, 9)}}`;
   }
   if (permission === 'ssn') {
@@ -55,6 +62,15 @@ const formatData = function (responseOb, permission) {
   }
   if (permission === 'birthday') {
     return permission;
+  }
+  if (permission === 'creditRating') {
+      return false;
+  }
+  if (permission === 'visaStanding') {
+      return true;
+  }
+  if (permission === 'income') {
+      return "$100,000+";
   }
   return null;
 };
